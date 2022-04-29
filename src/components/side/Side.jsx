ae174010816049c464cc688146f7a8606a190651
jowunseop1110 <a01083661364@gamil.com>
2022-04-29 13:13:17 +0900

    fdsfdfdsfdsfs

--- src/components/side/Side.jsx
+++ src/components/side/Side.jsx
@@ -4,16 +4,7 @@ import "./side.css";
 export default function Side() {
     return (
         <div className="side">
-            <div className="ad">
-                <iframe
-                    src="https://ads-partners.coupang.com/widgets.html?id=572081&template=carousel&trackingCode=AF5095775&subId=&width=300&height=300"
-                    width="300"
-                    height="300"
-                    frameBorder="0"
-                    scrolling="no"
-                    referrerPolicy="unsafe-url"
-                ></iframe>
-            </div>
+            <div className="ad"></div>
         </div>
     );
 }
