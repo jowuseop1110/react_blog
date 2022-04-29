fe5a8d251b01f7c72c8df6ee3bf1b5e231234c47
jowunseop1110 <a01083661364@gamil.com>
2022-04-29 12:29:07 +0900

    ractblog

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./post.css";
+
+export default function Post() {
+    return (
+        <article className="post">
+            <figure className="post__header" aria-hidden="true">
+                <img
+                    src="https://source.unsplash.com/800x580/?
+
+                    bikini"
+                    alt="dd"
+                />
+            </figure>
+            <div className="post__body">
+                <span className="post__cate">javascript</span>
+                <div className="post__title">포스트 제목입니다.</div>
+                <div className="post__desc">
+                    포스트 설명부분입니다. 포스트 설명부분입니다. 포스트 설명부분입니다. 포스트 설명부분입니다. 포스트
+                    설명부분입니다. 포스트 설명부분입니다. 스트 설명부분입니다. 스트 설명부분입니다.
+                </div>
+                <div className="post__info">
+                    <span className="author">조운섭</span>
+                    <span className="date">2022-04-28</span>
+                </div>
+            </div>
+        </article>
+    );
+}
---
+++ src/components/side/Side.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./side.css";
+
+export default function Side() {
+    return (
+        <div className="side">
+            <div className="ad">
+                <iframe
+                    src="https://ads-partners.coupang.com/widgets.html?id=572081&template=carousel&trackingCode=AF5095775&subId=&width=300&height=300"
+                    width="300"
+                    height="300"
+                    frameborder="0"
+                    scrolling="no"
+                    referrerpolicy="unsafe-url"
+                ></iframe>
+            </div>
+        </div>
+    );
+}
